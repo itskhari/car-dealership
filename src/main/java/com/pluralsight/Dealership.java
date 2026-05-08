7a9f4bf23cfdc078c7f25105c92baba8859d0e90
itkhari <mrkharij1@gmail.com>
2026-05-08 10:53:17 -0400

    added code within the dealership and vehicle classes

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,71 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name, address, phone;
+    private ArrayList<Vehicle> inventory;
+    // you did this because every dealership would need an inventory list; it would need to be a field.
+
+// constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+        inventory = new ArrayList<Vehicle>();
+        // you did this because when a new dealership object is made you want it to make its own new list to store variables
+    }
+// getters and setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+// methods
+    public List<Vehicle> getVehiclesByPrice (double min, double max) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByMakeModel (String make, String model) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByYear (int min, int max) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByColor (String color) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByMileage (int min, int max) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByType (String vehicleType) {
+        return null;
+    }
+    public void addVehicle(Vehicle newVehicle) {
+        inventory.add(newVehicle);
+    }
+    public void removeVehicle(Vehicle vehicleToRemove) {
+        // supposed to be empty for now I believe
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,83 @@
 package com.pluralsight;
 
 public class Vehicle {
+    private String make, model, color, vehicleType;
+    private int year, vin, odometer;
+    private double price;
+
+// constructor
+    public Vehicle(String make, String model, String color, String vehicleType, int year, int vin, int odometer, double price) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.vehicleType = vehicleType;
+        this.year = year;
+        this.vin = vin;
+        this.odometer = odometer;
+        this.price = price;
+    }
+// getter and setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
